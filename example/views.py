69cfee8a8534a11e452d0ad229209120ea5d0716
ammar-faifi <a@ammarf.com>
2022-04-29 13:45:57 +0300

    simple example

--- example/views.py
+++ example/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
 
-# Create your views here.
+
+class IndexView(TemplateView):
+
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['test_var'] = 'this a test var'
+        return context
