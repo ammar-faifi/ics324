10ea428d36bca6636a159ba6228c99d6b98adb74
ammar-faifi <a@ammarf.com>
2022-05-04 21:58:29 +0300

    use type hints

--- example/views.py
+++ example/views.py
@@ -1,3 +1,4 @@
+from typing import Any, Dict
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
 
@@ -5,8 +6,7 @@ from django.views.generic.base import TemplateView
 class IndexView(TemplateView):
 
     template_name = 'index.html'
-
-    def get_context_data(self, **kwargs):
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
         context = super().get_context_data(**kwargs)
         context['test_var'] = 'this a test var'
         return context
