c6e6eb68c0bc163f9e13e814b1c2557b96ba7339
ammar-faifi <a@ammarf.com>
2022-05-06 15:43:22 +0300

    white line

--- example/views.py
+++ example/views.py
@@ -6,6 +6,7 @@ from django.views.generic.base import TemplateView
 class IndexView(TemplateView):
 
     template_name = 'index.html'
+    
     def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
         context = super().get_context_data(**kwargs)
         context['test_var'] = 'this a test var'
