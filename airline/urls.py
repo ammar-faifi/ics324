fd86482c4af36e3399a930a316d51e124eb6342a
AliAbuRub <90558669+AliAbuRub@users.noreply.github.com>
2022-05-06 16:07:42 +0300

    flight-page

--- airline/urls.py
+++ airline/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('example/', include('example.urls'))
+    path('example/', include('example.urls')),
+    path('flight/', include('flight.urls'))
 ]
--- flight/views.py
+++ flight/views.py
@@ -1,3 +1,21 @@
+from typing import Any, Dict
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
 
 # Create your views here.
+
+
+class IndexView(TemplateView):
+
+    template_name = "index.html"
+
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context["test_var"] = "this a test var"
+        return context
+
+
+def check_booking(request):
+    print(request)
+    print(request.DATA)
+    return
