631ebabb8b8a4c831a01bcf22129d35a35e97c24
ammar-faifi <a@ammarf.com>
2022-05-06 16:46:41 +0300

    fix template naming

--- flight/views.py
+++ flight/views.py
@@ -7,7 +7,7 @@ from django.views.generic.base import TemplateView
 
 class IndexView(TemplateView):
 
-    template_name = "index.html"
+    template_name = "flight/index.html"
 
     def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
         context = super().get_context_data(**kwargs)
